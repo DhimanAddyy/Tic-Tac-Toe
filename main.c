71cbb628797f806dbc771e89d73613355ea8534d
DhimanAddyy <adityadhiman.2005@gmail.com>
2026-05-01 19:30:42 +0530

    Bug fix #1
    we got bigger bugs now.

--- main.c
+++ main.c
@@ -18,17 +18,21 @@ void display(int arr[3][3]){
 void selection(int arr[3][3], int n){
     int choice;
     int i, j;
-    printf("choice: ");
-    scanf("%d", &choice);
-    choice--;
-    j = choice % 3;
-    i = choice/3;
-    if(arr[i][j] == 0){
-        arr[i][j]=n;
-    }
-    else{
-        printf("Please choose an empty space");
-    }
+    do{
+        printf("choice: ");
+        scanf("%d", &choice);
+        choice--;
+        j = choice % 3;
+        i = choice/3;
+        if(arr[i][j] == 0){
+            arr[i][j]=n;
+            break;
+        }
+        else{
+            printf("Please choose an empty space\n");
+            display(arr);
+        }
+    }while(1);
 }
 
 int checkWin(int arr[3][3]){
@@ -37,6 +41,7 @@ int checkWin(int arr[3][3]){
     if(arr[i][j] == arr[i+1][j+1] && arr[i+1][j+1] == arr[i+2][j+2]){
         return arr[i][j];
     }
+
     for(i = 0, j = 0; i < 3; i++){
         if(arr[i][j] == arr[i][j+1] && arr[i][j+1] == arr[i][j+2]){
             return arr[i][j];
@@ -59,12 +64,13 @@ int checkWin(int arr[3][3]){
     return 0;
 }
 
-void replay(){
-    char replay;
-    printf("would you like to play again?(y/n)");
-        scanf("%c", &replay);
-        if (replay == 'y') {
-           main();
+char restart(){
+    char re;
+    printf("\nwould you like to play again?(y/n):");
+        getchar();
+        scanf("%c", &re);
+        if (re == 'y') {
+           return re;
         }else{
             printf("\nexiting program...");
         }
@@ -72,27 +78,35 @@ void replay(){
 
 int main(){
     int arr[3][3]={0};
-    display(arr);
     int winner, player, q=0;
-    do {for(player = 1; player < 3; player++){
+    char replay;
+    do {display(arr);
         while(q != 9){
-            printf("player %d move:-\n", player);
-            selection(arr, player);
-            display(arr);
-            q++;
-            winner = checkWin(arr);
-            if(winner != 0){
-                break;
-            }  
-            if(player == 2){
-                player = player-2;
+            for(player = 1; player < 3; player++){
+                printf("player %d move:-\n", player);
+                selection(arr, player);
+                display(arr);
+                q++;
+                winner = checkWin(arr);
+                //printf("winner = %d", winner);
+                if(winner != 0){
+                    break;
+                }  
+                //printf("%d", player);
+                if(player == 2){
+                    player = player-2;
+                }
             }
-        }
-        printf("\nDraw\n");
-        
         }
         if(winner !=0){ 
             printf("Player %d has won!", player);
-        }
-} while(winner == 0);
+        } else {printf("\nDraw\n");}
+        q = 0;
+        for(int i = 0;i < 3;i++){
+            for(int j = 0;j < 3;j++){
+                arr[i][j] = 0;
+            }
+        }    
+        replay = restart();
+    } while(replay == 'y');
 }
